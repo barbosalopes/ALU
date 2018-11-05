b8ac2370347cc38aa1255688cc5ed09367090275
Alef Henrique Martins Santos <1137286@sga.pucminas.br>
2018-11-04 23:04:12 -0200

    Partial Create ALU32bits

--- ALU/ALU/ALU32bits.cs
+++ ALU/ALU/ALU32bits.cs
@@ -14,7 +14,7 @@ namespace ALU
 
         /*		
                  OPCODES
-                F0	Saída
+                F3	Saída
                 0	A + B
                 1	A - B
         */
@@ -24,9 +24,48 @@ namespace ALU
 
         }
 
-        public bool Run(bool F0)
+        public bool Run(bool[] A, bool[] B, bool F0, bool carryIn, out bool carryOut)
         {
+            bool M0;
+            bool[] M1 = new bool[23];
+            bool[] M2 = new bool[8];
+
+            // Início bit de sinal
+
+            M0 = A1.Run(A[0], B[0], true, false, F0, carryIn, out carryOut); 
             
+            // Fim bit de sinal
+
+            // Início mantissa fracionária
+
+            bool[] IA23 = new bool[23];
+            bool[] IB23 = new bool[23];
+
+            for (int i = 0, aux = 1; i < 24; i++, aux++)
+            {
+                IA23[i] = A[aux];
+                IB23[i] = B[aux];
+            }
+
+            M1 = A23.Run(IA23, IB23, true, false, F0, carryIn, out carryOut); 
+            
+            // Fim mantissa fracionária
+
+            // Início expoente polarizado
+
+            bool[] IA8 = new bool[8];
+            bool[] IB8 = new bool[8];
+
+            for (int i = 0, aux = 25; i < 8; i++, aux++)
+            {
+                IA8[i] = A[aux];
+                IB8[i] = B[aux];
+            }
+
+            M2 = A8.Run(IA8, IB8, true, false, F0, carryIn, out carryOut); 
+            
+            // Fim expoente polarizado
+
             return false;
         }
     }
